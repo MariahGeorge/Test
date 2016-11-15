9a1c21324348fc728a4ca1ef8da5fe923a512209
Yearup <yearup@macimage-ac87a30c2347.local>
2016-11-15 15:11:31 -0800

    added an h3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('h2').html("Join me as we take a look at music!");
 	$('h2').css("color", "grey");
 	$('h1').css("color", "pink");
+	$('h3').css("color", "grey");
 
 
 });
